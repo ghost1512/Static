33a2f5c181cc47c5ae30597890f0ec517af80023
BaoHG <baohuynh291@gmail.com>
2024-04-16 19:01:23 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Add();
+    }
+
+    static void Add()
+    {
+        Console.WriteLine($"Truong cua ban la: {SinhVien.School}");
+        SinhVien sv = new SinhVien();
+        Console.WriteLine($"Lop cua ban la: {sv.myClass}");
+    }
+}
+class SinhVien
+{
+    public string myClass = "CNTT";
+    public static string School = "fpt";
+
+}
